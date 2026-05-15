42995118243768cc2a1c6906de6a880b81af412c
Mo'mina <hamidullayevna.06@gmail.com>
2026-05-15 15:58:55 +0500

    fixed production api

--- src/services/api.js
+++ src/services/api.js
@@ -5,12 +5,15 @@ const options = {
   headers: {
     accept: "application/json",
     Authorization:
-      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Y2QxODkyNzMzMTdmMWZjODA5Yzc4ZmJlZDllNWE0OSIsIm5iZiI6MTc3ODY4ODUzMy40MTkwMDAxLCJzdWIiOiI2YTA0YTIxNTBlZjdiMTRhMjc3NGQzYWQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.139u755z7LrQ7zGlJ3K0jGEScDkppayf_LQYbNCTSfM ",
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Y2QxODkyNzMzMTdmMWZjODA5Yzc4ZmJlZDllNWE0OSIsIm5iZiI6MTc3ODY4ODUzMy40MTkwMDAxLCJzdWIiOiI2YTA0YTIxNTBlZjdiMTRhMjc3NGQzYWQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.139u755z7LrQ7zGlJ3K0jGEScDkppayf_LQYbNCTSfM",
   },
 };
 
 export const getPopularMovies = async () => {
-  const response = await fetch(`${BASE_URL}/movie/popular`, options);
+  const response = await fetch(
+    `${BASE_URL}/movie/popular`,
+    options
+  );
 
   const data = await response.json();
 
@@ -20,7 +23,7 @@ export const getPopularMovies = async () => {
 export const searchMovies = async (query) => {
   const response = await fetch(
     `${BASE_URL}/search/movie?query=${query}`,
-    options,
+    options
   );
 
   const data = await response.json();
