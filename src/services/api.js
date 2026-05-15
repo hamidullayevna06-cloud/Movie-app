723c0703247d2735ba3f2cafa0a24c4c7754013f
Mo'mina <hamidullayevna.06@gmail.com>
2026-05-15 15:48:11 +0500

    fixed tmdb auth

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,16 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
 const BASE_URL = "https://api.themoviedb.org/3";
 
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Y2QxODkyNzMzMTdmMWZjODA5Yzc4ZmJlZDllNWE0OSIsIm5iZiI6MTc3ODY4ODUzMy40MTkwMDAxLCJzdWIiOiI2YTA0YTIxNTBlZjdiMTRhMjc3NGQzYWQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.139u755z7LrQ7zGlJ3K0jGEScDkppayf_LQYbNCTSfM ",
+  },
+};
+
 export const getPopularMovies = async () => {
-  const response = await fetch(
-    `${BASE_URL}/movie/popular?api_key=${API_KEY}`
-  );
+  const response = await fetch(`${BASE_URL}/movie/popular`, options);
 
   const data = await response.json();
 
@@ -14,7 +19,8 @@ export const getPopularMovies = async () => {
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+    `${BASE_URL}/search/movie?query=${query}`,
+    options,
   );
 
   const data = await response.json();
